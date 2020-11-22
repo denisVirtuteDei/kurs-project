d85697ec611c96126d179c103efc297e12bf3529
Igor Shark <sharkevich.i@inbox.ru>
2020-11-22 19:09:14 +0300

    edit bg img

--- src/About.js
+++ src/About.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import Jumbotron from './Components/Jumbotron';
-import {Dropdown} from 'react-bootstrap';
+import {Container} from 'react-bootstrap'
 
-export const About = () =>  (
-   
-    
-    
-    <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
-  </Dropdown.Toggle>
 
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown>
-    
-)
+export const About = () =>  ( 
+
+  <Container></Container>
+  )
